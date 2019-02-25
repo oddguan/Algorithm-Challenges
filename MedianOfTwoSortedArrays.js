4a7433a562c7178be36723f471a2c2d40116cb63
Chenxiao Guan <1011zaozao@gmail.com>
2019-02-25 18:55:33 -0500

    Median of two sorted arrays

---
+++ MedianOfTwoSortedArrays.js
@@ -0,0 +1,43 @@
+class Solution {
+  findMedianSortedArrays(nums1, nums2) {
+    const len1 = nums1.length;
+    const len2 = nums2.length;
+    if (len1 > len2) {
+      return findMedianSortedArrays(nums2, nums1);
+    }
+
+    const len = len1 + len2;
+    if (!(len & 0x01)) { // even
+      return (this.findKth(nums1, 0, nums2, 0, len1, len2, parseInt(len/2)) + 
+      this.findKth(nums1, 0, nums2, 0, len1, len2, parseInt(len/2)+1)) / 2;
+    } else {
+      return this.findKth(nums1, 0, nums2, 0, len1, len2, parseInt(len/2)+1);
+    }
+  };
+
+  findKth(nums1, start1, nums2, start2, len1, len2, k) {
+    if (len1 === 0) {
+      return nums2[start2 + k - 1];
+    }
+    if (len2 === 0) {
+      return nums1[start1 + k - 1];
+    }
+    if (k === 1) {
+      return Math.min(nums1[start1], nums2[start2]);
+    }
+    const p = Math.min(Math.max(1, parseInt(k / 2)), len1);
+    const q = k - p;
+    if (nums1[start1 + p - 1] === nums2[start2 + q - 1]) {
+      return nums1[start1 + p - 1];
+    } else if (nums1[start1 + p - 1] > nums2[start2 + q - 1]) {
+      return this.findKth(nums1, start1, nums2, start2 + q, len1, len2 - q, k - q); 
+    } else {
+      return this.findKth(nums1, start1 + p, nums2, start2, len1 - p, len2, k - p);
+    }
+  };
+}
+
+s1 = new Solution();
+// console.log(s1.findMedianSortedArrays([2], [1,3]));
+console.log(s1.findMedianSortedArrays([10001, 10002, 10007], [10000, 10004, 10006]));
+// console.log((2+3)/2);
--- test.cpp
+++ test.cpp
@@ -1,40 +1,50 @@
-#include <iostream>       // std::cout
-#include <stack>          // std::stack
-#include <vector>         // std::vector
-#include <deque>          // std::deque
-#include <string>
+#include <vector>
 
-using namespace std;
 class Solution {
+private:
+    double findKth(vector<int>& nums1, vector<int>& nums2, int start1, int len1, int start2, int len2, int k) {
+        if (len1 > len2) {
+            return findKth(nums2, nums1, start2, len2, start1, len1, k);
+        }
+
+        if (len1 == 0) {
+            return nums2[start2 + k - 1];
+        }
+
+        if (k == 1) {
+            return min(nums1[start1], nums2[start2]);
+        }
+
+        int p1 = min(k / 2, len1);
+        int p2 = k - p1;
+        if (nums1[start1 + p1 - 1] > nums2[start2 + p2 - 1]) {
+            return findKth(nums1, nums2, start1, len1, start2 + p2, len2 - p2, k - p2);
+        }
+        else if(nums1[start1 + p1 - 1] < nums2[start2 + p2 - 1]){
+            return findKth(nums1, nums2, start1 + p1, len1 - p1, start2, len2, k - p1);
+        }
+        else {
+            return nums1[start1 + p1 - 1];
+        }
+
+    }
+
 public:
-bool isValid(string s) {
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int len = nums1.size() + nums2.size();
 
-	stack<char> stk;
-	for (auto i = 0; i < s.size(); i++) {
-		
-		switch (s[i]) {
-			case '(': stk.push(s[i]); break;
-			case '[': stk.push(s[i]); break;
-			case '{': stk.push(s[i]); break;
-
-			case ')': if (stk.empty()) return false;
-					  if (stk.top() != '(') return false;
-					  else stk.pop(); break;
-			case ']': if (stk.empty()) return false;
-					  if (stk.top() != '[') return false;
-					  else stk.pop(); break;
-			case '}': if (stk.empty()) return false;
-					  if (stk.top() != '{') return false;
-					  else stk.pop(); break;
-
-			default: break;
-		}
-	}
-	return stk.empty();
-}
+        if (!(len & 0x01)) {
+            return (findKth(nums1, nums2, 0, nums1.size(), 0, nums2.size(), len / 2)
+                + findKth(nums1, nums2, 0, nums1.size(), 0, nums2.size(), len / 2 + 1)
+                ) / 2.0f;
+        }
+        else {
+            return findKth(nums1, nums2, 0, nums1.size(), 0, nums2.size(), len / 2 + 1);
+        }
+    }
 };
 
-int main() {
-    Solution s;
-    cout << s.isValid("([])");
+int main(void) {
+	Solution s1 = new Solution();
+	cout << s1.findMedianSortedArrays([1,2,3], [4,5,6]);
 }
